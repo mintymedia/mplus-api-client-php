8a55b002596920afc77466992ae2931cf4d5c8f9
minty-isaac <isaac@mintymail.nl>
2019-10-24 13:36:56 +0200

    :rocket: Small tweaks & update to upstream

--- Mplusqapiclient.php
+++ Mplusqapiclient.php
@@ -372,7 +372,7 @@ class MplusQAPIclient
       // Don't wait longer than 5 seconds for the headers.
       // We call get_headers() here because we want a relatively fast check if the API is available at all
       // , before we actually initialize the SoapClient and start running requests
-      ini_set('default_socket_timeout', 5);
+      ini_set('default_socket_timeout', 20);
       if (false === @get_headers($wsdl_url)) {
         throw new MplusQAPIException(sprintf('Cannot find API WSDL @ %s', $wsdl_url));
       }
